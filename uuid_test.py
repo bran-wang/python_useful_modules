abc1482a9c993b95e0228a9337c8d17e33ae067f
bran-wang <branw@vmware.com>
2017-06-16 00:21:58 +0800

    identity UUID or name

--- uuid_test.py
+++ uuid_test.py
@@ -1,7 +1,14 @@
 import uuid
+from oslo_utils import uuidutils
 
-
-print dir(uuid)
+#print dir(uuid)
 
 cluster_id = str(uuid.uuid4())
 print cluster_id
+
+if uuidutils.is_uuid_like(cluster_id):
+    print "cluster id is UUID\n"
+else:
+    print "cluster id is not UUID\n"
+
+
